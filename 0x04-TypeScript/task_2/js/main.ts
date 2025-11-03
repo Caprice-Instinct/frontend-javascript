0942bfd98fa2e763cee7f1b8880d71e2d1e09142
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-03 21:13:28 +0300

    Fix: Export isDirector function, fix parameter types, declare functions in crud.d.ts, use spread operator, fix setTeacher signature, add task_5

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,7 +45,7 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
@@ -58,7 +58,7 @@ function executeWork(employee: Director | Teacher): string {
 
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
   } else {
