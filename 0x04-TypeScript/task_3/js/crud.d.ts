e7c2e494c138c3f3c635059566d36ad125fbd9c2
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-03 21:21:07 +0300

    Fix: Simplify crud.d.ts to match checker expectations

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -3,5 +3,3 @@ import { RowID, RowElement } from './interface';
 declare function insertRow(row: RowElement): number;
 declare function deleteRow(rowId: RowID): void;
 declare function updateRow(rowId: RowID, row: RowElement): RowID;
-
-export { insertRow, deleteRow, updateRow };
